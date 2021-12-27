2ba767d6e200c5c0e36ba499a67fc7a26acef0a0
Nim1com <57798165+Nim1com@users.noreply.github.com>
2021-12-27 16:27:25 +0200

    Update items.js

--- src/assets/items.js
+++ src/assets/items.js
@@ -6,6 +6,7 @@ import dribbble from '../assets/dribbble.png'
 import github from '../assets/github.png'
 import telegram from '../assets/telegram.png'
 import twitter from '../assets/twitter.png'
+import discord from '../assets/discord.png'
 
 const items = [{
     "title": "Website",
@@ -43,6 +44,13 @@ const items = [{
     "subtitle": "@virtuonic | Hub to the Shots of my 😎 apps ",
     "image": dribbble,
     "link": "https://dribbble.com/virtuonic" // Dribbble profile link 
+},
+
+{
+    "title": "Discord",
+    "subtitle": "0xNim.eth#1129 | My main social place ",
+    "image": discord,
+    "link": "https://discord.com/" // Dribbble profile link 
 }]
 
 export default items
