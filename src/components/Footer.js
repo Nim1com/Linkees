b39b7c311dcbe65c7111aff677d0b4bee70cabbd
Nim1com <57798165+Nim1com@users.noreply.github.com>
2021-12-28 12:33:34 +0200

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,6 +7,7 @@ import '../css/components.css'
 function Footer() {
     return (
         <div className="Footer container">
+            <script async src="https://arc.io/widget.min.js#YpMu3B8u"></script>
             <p>Hosted with <span class="heart">❤️</span> by <a href={"https://twitter.com/Nimq___"}>0xNim</a></p>
         </div>
     )
